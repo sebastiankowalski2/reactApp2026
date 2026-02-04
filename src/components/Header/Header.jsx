5be535dcd5bb40d97c49cad3a633f05eac91a64d
sebastiankowalski2 <sebastian.kowalski20020@gmail.com>
2026-02-04 18:17:46 +0100

    second commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css'
+
+function Header() {
+  return (
+    <header>
+      <h1 className="title">Welcome to My React App</h1>
+      <p className="subtitle">Siemanko, grypsujesz?</p>
+    </header>
+  )
+}
+
+export default Header
